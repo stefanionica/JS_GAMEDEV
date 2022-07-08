41ac692a4be84465739d53e6628ecfdd547c2923
Stefan Ionica <stefan.ionica@histria.lan>
2022-07-08 15:46:01 +0300

    curs_01

---
+++ New folder/curs_01 (2)/curs_01/hello-vue/src/components/HelloWorld.vue
@@ -0,0 +1,159 @@
+<template>
+  <article>This is my article</article>
+  <div class="hello">
+    <Transition name="fade" mode="out-in">
+      <div>
+        <div v-if="age > 20">{{ name }} ({{ age }} yrs old)</div>
+        <button v-if="isOn" @click="isOn = !isOn" key="on">On</button>
+        <button v-else @click="isOn = !isOn" key="off">Off</button>
+      </div>
+    </Transition>
+
+    <div class="figure">
+      <img src="https://picsum.photos/536/354" />
+    </div>
+    <p class="animate__animated animate__jello">{{ title }}</p>
+    <h1 class="animate__animated animate__wobble">{{ msg }}</h1>
+    <p id="firstParag">
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+          >eslint</a
+        >
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+          >vue-devtools</a
+        >
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import "animate.css";
+import anime from "animejs/lib/anime.es.js";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      title: "hello from vue",
+      age: 22,
+      name: "Gigel",
+      isOn: false,
+    };
+  },
+};
+
+window.onload = function () {
+  anime({
+    targets: "img",
+    width: "200%",
+    easing: "easeInOutQuad",
+    direction: "alternate",
+    loop: true,
+  });
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#firstParag {
+  color: blue;
+  font-weight: bold;
+  letter-spacing: 2px;
+
+  animation: headShake; /* referring directly to the animation's @keyframe declaration */
+  animation-duration: 2s; /* don't forget to set a duration! */
+}
+div.figure {
+  background-color: gray;
+}
+</style>
