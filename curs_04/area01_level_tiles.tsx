3904ea8538537028623edf9fdb0e988ec764d15f
Stefan Ionica <stefan.ionica@histria.lan>
2022-07-22 08:39:52 +0300

    JS_GAMEDEV

---
+++ curs_04/area01_level_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="area01_level_tiles" tilewidth="32" tileheight="32" tilecount="140" columns="20">
+ <image source="hello-mel/src/data/img/map/area01_level_tiles.png" width="640" height="241"/>
+</tileset>
